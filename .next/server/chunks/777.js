a8718272459c4a94c883c832af5f9523e4abf8de
reubence <reuben3699@gmail.com>
2022-01-15 11:10:36 +0530

    Modal Data

---
+++ .next/server/chunks/777.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.id = 777;
+exports.ids = [777];
+exports.modules = {
+
+/***/ 6777:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "O": () => (/* binding */ supabase)
+/* harmony export */ });
+/* harmony import */ var _supabase_supabase_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2885);
+/* harmony import */ var _supabase_supabase_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_supabase_supabase_js__WEBPACK_IMPORTED_MODULE_0__);
+
+const supabaseUrl = (/* unused pure expression or super */ null && ("https://hyochqpwwazbblusjyjk.supabase.co"));
+const supabaseAnonKey = (/* unused pure expression or super */ null && ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTI5ODk2NywiZXhwIjoxOTU2ODc0OTY3fQ.PitQxLCn_ex-5dfAs8JMcAeu8YHWx4BvaZpFomecc-k"));
+const supabase = (0,_supabase_supabase_js__WEBPACK_IMPORTED_MODULE_0__.createClient)("https://hyochqpwwazbblusjyjk.supabase.co" || 0, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTI5ODk2NywiZXhwIjoxOTU2ODc0OTY3fQ.PitQxLCn_ex-5dfAs8JMcAeu8YHWx4BvaZpFomecc-k" || 0);
+
+
+/***/ })
+
+};
+;
