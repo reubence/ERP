fc70a0f0cc2d0817e333174d06004bcbd4a74d2a
reubence <reuben3699@gmail.com>
2022-02-15 18:30:26 +0530

    v1.2.4 - Fixed Typescript Issues from v1.2.3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e65cad925027fe5"; }
+/******/ 		__webpack_require__.h = function() { return "fc7f600bb2def7ed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/HigherOrderComponents/ModalHOC.tsx
+++ components/HigherOrderComponents/ModalHOC.tsx
@@ -1,7 +1,13 @@
 import { useRef, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
-function ModalHOC({ children, selector }) {
+function ModalHOC({
+  children,
+  selector,
+}: {
+  children: React.ReactChild;
+  selector: string;
+}) {
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
