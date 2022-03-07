e6828d1699798281bdafef6b6b6e97213a54a3a9
reubence <reuben3699@gmail.com>
2022-03-07 17:16:53 +0530

    Testing 3

--- pages/api/downloadcsv.ts
+++ pages/api/downloadcsv.ts
@@ -1,22 +1,22 @@
-// import { supabase } from "../../utils/supabaseClient";
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { CSVLink, CSVDownload } from "react-csv";
-// import XLSX from "xlsx";
+import { supabase } from "../../utils/supabaseClient";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CSVLink, CSVDownload } from "react-csv";
+import XLSX from "xlsx";
 
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const { data, error } = await supabase.from("ledger").select("*");
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { data, error } = await supabase.from("ledger").select("*");
 
-//   if (error) throw new Error(`unexpected response ${error.message}`);
-//   console.log(data);
-//   const downloadExcel = () => {
-//     const worksheet = XLSX.utils.json_to_sheet(data);
-//     const workbook = XLSX.utils.book_new();
-//     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-//     //let buffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
-//     //XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
-//     XLSX.writeFile(workbook, "DataSheet.xlsx");
-//   };
-//   downloadExcel;
+  //   if (error) throw new Error(`unexpected response ${error.message}`);
+  //   console.log(data);
+  //   const downloadExcel = () => {
+  //     const worksheet = XLSX.utils.json_to_sheet(data);
+  //     const workbook = XLSX.utils.book_new();
+  //     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+  //     //let buffer = XLSX.write(workbook, { bookType: "xlsx", type: "buffer" });
+  //     //XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
+  //     XLSX.writeFile(workbook, "DataSheet.xlsx");
+  //   };
+  //   downloadExcel;
 
-//   return res.status(200).json(data);
-// };
+  return res.status(200).json(data);
+};
